e15cdcf0ad372400ba81ed8286142abde4d8f1c7
Win122333 <artem.chulkov.90@mail.ru>
2024-12-06 15:02:21 +0300

    Начало положено

---
+++ src/main/java/com/cgvsu/render_engine/Normals.java
@@ -0,0 +1,9 @@
+package com.cgvsu.render_engine;
+
+import com.cgvsu.model.Model;
+
+public class Normals {
+    public static void findNormals(Model model) {
+        System.out.println("нащел нормали");
+    }
+}
---
+++ src/main/java/com/cgvsu/render_engine/Triangulation.java
@@ -0,0 +1,9 @@
+package com.cgvsu.render_engine;
+
+import com.cgvsu.model.Model;
+
+public class Triangulation {
+    public static Model triangulate(Model model) {
+        return null;
+    }
+}
